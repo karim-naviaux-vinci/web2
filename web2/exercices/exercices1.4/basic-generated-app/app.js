f70544779072e9fd8b4b9f89c72e04b8c27d7728
karim-naviaux <karimnaviaux03@gmail.com>
2023-09-25 12:07:11 +0200

    adding tutorials-template

--- web2/exercices/exercices1.4/basic-generated-app/app.js
+++ web2/exercices/exercices1.4/basic-generated-app/app.js
@@ -28,5 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/pizzas', pizzasRouter);
+app.use('/films', filmsRouter);
 
 module.exports = app;
