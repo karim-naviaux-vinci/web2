b81a0b0ec47be5774dbc5d2c17526eb8207be40e
karim-naviaux <karim.naviaux@student.vinci.be>
2023-10-01 16:43:11 +0200

    maj dossier exercices1.5

---
+++ web2/exercices/exercices1.5/basic-generated-app/routes/films.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var router = express.Router();
+
+const CATALOGUE_FILM = [
+    {
+        id:1,
+        title:'pulp fiction',
+        duration : 153,
+        budget : 100000000,
+        link : 'https://en.wikipedia.org/wiki/Pulp_Fiction'
+    },
+
+    {
+        id:2,
+        title:'Superman : man of steel',
+        duration : 143,
+        budget : 256000000,
+        link : 'https://en.wikipedia.org/wiki/Man_of_Steel_(film)'
+    },
+
+    {
+        id:3,
+        title:'Avengers : Infinity war',
+        duration : 149,
+        budget : 325000000,
+        link : 'https://www.imdb.com/title/tt4154756/'
+    }
+];
+
+router.get('/', (req, res, next) => {
+    // Extraction de la valeur "minimum-duration" depuis les paramètres de la requête
+    const minimum_value = req.query['minimum-duration'];
+
+    console.log(minimum_value);
+
+    let orderedCatalogue;
+
+    if (minimum_value && !isNaN(minimum_value)) {
+        // Si "minimum_value" est un nombre et supérieur ou égal à 143,
+        // trier le catalogue par la durée en utilisant localeCompare
+        orderedCatalogue = [...CATALOGUE_FILM].sort((a, b) => String(a.duration).localeCompare(String(b.duration))); //localecompare est utiliser pour comparer des valeur String
+    } else {
+        // Si "minimum_value" n'est pas valide, retourner le catalogue d'origine
+        orderedCatalogue = CATALOGUE_FILM;
+    }
+
+    // Répondre avec le catalogue trié ou le catalogue d'origine
+    res.json(orderedCatalogue);
+});
+
+
+
+router.get('/:id', (req, res) => {
+    console.log('id : ' + req.params.id);
+
+    const indexFound = CATALOGUE_FILM.findIndex((film) => film.id == req.params.id);
+
+    if(indexFound < 0) return res.sendStatus(404);
+
+    res.json(CATALOGUE_FILM[indexFound]);
+})
+
+router.post('/', (req, res) => {
+    const title = typeof req?.body?.title === 'string' ? req.body.title : undefined;
+    const duration = typeof req?.body?.duration === 'number' && req.body.duration >= 60 ? req.body.duration : undefined;
+    const budget = typeof req?.body?.budget === 'number' && req.body.budget > 0 ? req.body.budget : undefined;
+    const link = typeof req?.body?.link === 'string' ? req.body.link : undefined;
+
+    console.log(title + " " + duration + " " + budget + " " + link)
+
+    if(!title || !duration || !budget || !link) return res.sendStatus(404);
+
+    const lastId = CATALOGUE_FILM?.length !== 0 ? CATALOGUE_FILM[CATALOGUE_FILM.length - 1].id : undefined;
+    const nextIndex = lastId + 1;
+
+    console.log(lastId);
+    console.log(nextIndex);
+
+    const newFilm = {
+        id : nextIndex,
+        title : title,
+        duration : duration,
+        budget : budget,
+        link : link
+    };
+
+    for (const film of CATALOGUE_FILM) {
+        if(film.title == newFilm.title){
+            return res.sendStatus(409)
+        }
+    }
+
+    CATALOGUE_FILM.push(newFilm);
+    res.json(newFilm);
+
+})
+
+module.exports = router;
